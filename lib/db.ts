403a22bf885e687c3fd1b82db50b5f35b540d3ee
Sahil <sahil2flamingo@gmail.com>
2026-03-04 09:18:58 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 
-const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:sahil34@@localhost:5432/urlshortener';
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/urlshortener';
 
 export const pool = new Pool({
   connectionString: databaseUrl,
